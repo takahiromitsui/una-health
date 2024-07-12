64beaedd40c3fe87242c0819ccbbf996b370b7c9
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-12 14:20:40 +0200

    feat: add select by id

--- backend/restapi/restapi/glucose_levels/views.py
+++ backend/restapi/restapi/glucose_levels/views.py
@@ -1,16 +1,68 @@
+from datetime import datetime
+from django.utils.timezone import make_aware
 from rest_framework import viewsets
 from rest_framework.response import Response
-from drf_spectacular.utils import extend_schema
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.types import OpenApiTypes
+from django.shortcuts import get_object_or_404
 
 from .models import GlucoseLevel
 from .serializers import GlucoseLevelSerializer
 
 
 # Create your views here.
-class GlucoseLevelViewSet(viewsets.ViewSet):
-    queryset = GlucoseLevel.objects.all()
+# class GlucoseLevelViewSet(viewsets.ViewSet):
+#     queryset = GlucoseLevel.objects.all()
 
-    @extend_schema(responses=GlucoseLevelSerializer)
-    def list(self, request):
-        serializer = GlucoseLevelSerializer(self.queryset, many=True)
+#     @extend_schema(
+#         responses=GlucoseLevelSerializer,
+#         parameters=[
+#             OpenApiParameter(
+#                 name="user_id",
+#                 description="Filter by user ID",
+#                 required=False,
+#                 type=OpenApiTypes.STR,
+#             ),
+#             OpenApiParameter(
+#                 name="start",
+#                 description="Start timestamp for filtering",
+#                 required=False,
+#                 type=OpenApiTypes.DATETIME,
+#             ),
+#             OpenApiParameter(
+#                 name="stop",
+#                 description="Stop timestamp for filtering",
+#                 required=False,
+#                 type=OpenApiTypes.DATETIME,
+#             ),
+#         ],
+#     )
+#     def list(self, request):
+#         user_id = request.query_params.get("user_id")
+#         if user_id:
+#             self.queryset = self.queryset.filter(user_id=user_id)
+#         start = request.query_params.get("start")
+#         stop = request.query_params.get("stop")
+#         if start and stop:
+#             start_date = make_aware(datetime.strptime(start, "%Y-%m-%dT%H:%M:%S"))
+#             stop_date = make_aware(datetime.strptime(stop, "%Y-%m-%dT%H:%M:%S"))
+#             self.queryset = self.queryset.filter(
+#                 timestamp__range=(start_date, stop_date)
+#             )
+#         sort = request.query_params.get("sort", "timestamp")
+#         self.queryset = self.queryset.order_by(sort)
+
+#         serializer = GlucoseLevelSerializer(self.queryset, many=True)
+#         return Response(serializer.data)
+
+
+class GlucoseLevelViewSet(viewsets.ViewSet):
+
+    @extend_schema(
+        responses=GlucoseLevelSerializer,
+    )
+    def retrieve(self, request, pk=None):
+        queryset = GlucoseLevel.objects.all()
+        item = get_object_or_404(queryset, pk=pk)
+        serializer = GlucoseLevelSerializer(item)
         return Response(serializer.data)
