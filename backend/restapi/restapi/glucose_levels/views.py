b5c35a7ea42aeeb7382bd008d12ce9145dfdf8a0
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-12 12:27:56 +0200

    feat: create the first view for levels

--- backend/restapi/restapi/glucose_levels/views.py
+++ backend/restapi/restapi/glucose_levels/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
+
+from .models import GlucoseLevel
+from .serializers import GlucoseLevelSerializer
+
 
 # Create your views here.
+class GlucoseLevelViewSet(viewsets.ModelViewSet):
+    queryset = GlucoseLevel.objects.all()
+
+    @extend_schema(responses=GlucoseLevelSerializer)
+    def list(self, request):
+        serializer = GlucoseLevelSerializer(self.queryset, many=True)
+        return Response(serializer.data)
