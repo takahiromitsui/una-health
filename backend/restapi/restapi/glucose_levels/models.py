ce11d35b4706cb395f16eeeef5ed3c02cd34bd31
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-12 11:51:39 +0200

    feat: create user and glucose level model

--- backend/restapi/restapi/glucose_levels/models.py
+++ backend/restapi/restapi/glucose_levels/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_id = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.user_id
+
+
+class GlucoseLevel(models.Model):
+    # If a user is deleted, all glucose levels associated with that user should be deleted as well.
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    device = models.CharField(max_length=255)  # Gerät
+    serial_number = models.CharField(max_length=255)  # Seriennummer
+    device_timestamp = models.DateTimeField()  # Gerätezeitstempel
+    record_type = models.IntegerField()  # Aufzeichnungstyp
+    glucose_value_history = models.FloatField(
+        null=True, blank=True
+    )  # Glukosewert-Verlauf mg/dL
+
+    def __str__(self):
+        return f"{self.user.user_id} - {self.device} - {self.device_timestamp}"
