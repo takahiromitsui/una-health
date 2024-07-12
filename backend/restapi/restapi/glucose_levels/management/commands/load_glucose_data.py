4333e307cf1f7fb6e1c3f8331f8518aa401f00d4
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-12 13:25:00 +0200

    feat: load data

--- backend/restapi/restapi/glucose_levels/management/commands/load_glucose_data.py
+++ backend/restapi/restapi/glucose_levels/management/commands/load_glucose_data.py
@@ -1,6 +1,9 @@
+from datetime import datetime
 import pandas as pd
 import os
 from django.core.management.base import BaseCommand, CommandParser
+
+from restapi.glucose_levels.models import User, GlucoseLevel
 
 
 # poetry run python manage.py load_glucose_data ./restapi/glucose_levels/data_files/
@@ -15,6 +18,22 @@ class Command(BaseCommand):
         for user_file in os.listdir(file_path):
             if user_file.endswith(".csv"):
                 with open(os.path.join(file_path, user_file), "r") as f:
-                    df = pd.read_csv(f)
-                    print(df.head())
-                    print(user_file)
+                    df = pd.read_csv(f, skiprows=1)
+                    user_id = user_file.split(".")[0]
+                    user, _ = User.objects.get_or_create(user_id=user_id)
+                for _, row in df.iterrows():
+                    GlucoseLevel.objects.create(
+                        user=user,
+                        device=row["Gerät"],
+                        serial_number=row["Seriennummer"],
+                        device_timestamp=datetime.strptime(
+                            row["Gerätezeitstempel"], "%d-%m-%Y %H:%M"
+                        ),
+                        record_type=int(row["Aufzeichnungstyp"]),
+                        glucose_value_history=(
+                            float(row["Glukosewert-Verlauf mg/dL"])
+                            if row["Glukosewert-Verlauf mg/dL"]
+                            else None
+                        ),
+                    )
+        self.stdout.write(self.style.SUCCESS("Successfully loaded glucose data"))
