a710c60543109b586c7b6abc0b4b4a2d013d2baf
Takahiro Mitsui <takahiro.mitsui@code.berlin>
2024-07-12 14:45:05 +0200

    feat: add pagination

--- backend/restapi/restapi/glucose_levels/views.py
+++ backend/restapi/restapi/glucose_levels/views.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from django.utils.timezone import make_aware
 from rest_framework import viewsets
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from drf_spectacular.utils import extend_schema, OpenApiParameter
 from drf_spectacular.types import OpenApiTypes
 from django.shortcuts import get_object_or_404
@@ -10,53 +11,14 @@ from .models import GlucoseLevel
 from .serializers import GlucoseLevelSerializer
 
 
-# Create your views here.
-# class GlucoseLevelViewSet(viewsets.ViewSet):
-#     queryset = GlucoseLevel.objects.all()
-
-#     @extend_schema(
-#         responses=GlucoseLevelSerializer,
-#         parameters=[
-#             OpenApiParameter(
-#                 name="user_id",
-#                 description="Filter by user ID",
-#                 required=False,
-#                 type=OpenApiTypes.STR,
-#             ),
-#             OpenApiParameter(
-#                 name="start",
-#                 description="Start timestamp for filtering",
-#                 required=False,
-#                 type=OpenApiTypes.DATETIME,
-#             ),
-#             OpenApiParameter(
-#                 name="stop",
-#                 description="Stop timestamp for filtering",
-#                 required=False,
-#                 type=OpenApiTypes.DATETIME,
-#             ),
-#         ],
-#     )
-#     def list(self, request):
-#         user_id = request.query_params.get("user_id")
-#         if user_id:
-#             self.queryset = self.queryset.filter(user_id=user_id)
-#         start = request.query_params.get("start")
-#         stop = request.query_params.get("stop")
-#         if start and stop:
-#             start_date = make_aware(datetime.strptime(start, "%Y-%m-%dT%H:%M:%S"))
-#             stop_date = make_aware(datetime.strptime(stop, "%Y-%m-%dT%H:%M:%S"))
-#             self.queryset = self.queryset.filter(
-#                 timestamp__range=(start_date, stop_date)
-#             )
-#         sort = request.query_params.get("sort", "timestamp")
-#         self.queryset = self.queryset.order_by(sort)
-
-#         serializer = GlucoseLevelSerializer(self.queryset, many=True)
-#         return Response(serializer.data)
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_query_param = "page"
+    page_size_query_param = "page_size"
 
 
 class GlucoseLevelViewSet(viewsets.ViewSet):
+    pagination_class = CustomPagination
 
     @extend_schema(
         responses=GlucoseLevelSerializer,
@@ -84,8 +46,10 @@ class GlucoseLevelViewSet(viewsets.ViewSet):
             queryset = GlucoseLevel.objects.filter(user__user_id=user_id).order_by(
                 "device_timestamp"
             )
+            paginator = self.pagination_class()
+            page = paginator.paginate_queryset(queryset, request)
             if queryset.exists():
-                serializer = GlucoseLevelSerializer(queryset, many=True)
+                serializer = GlucoseLevelSerializer(page, many=True)
                 return Response(serializer.data)
             else:
                 return Response(
--- backend/restapi/restapi/settings/base.py
+++ backend/restapi/restapi/settings/base.py
@@ -84,6 +84,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
+    "PAGE_SIZE": 10,
 }
 
 SPECTACULAR_SETTINGS = {
